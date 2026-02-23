d5231cdd724340f120abfa677ef892ee67d3bbb1
Nanthakumar <nnantha971@gmail.com>
2026-02-23 17:29:30 +0530

    the logical and conditional oparators

---
+++ Day-31/07_conditionals.py
@@ -0,0 +1,57 @@
+is_raining = True
+
+if is_raining:
+    print("Wear raincoat 🥼")
+    print("Inside If")
+else:
+    print("Wear shorts 🩳 & Glasses 🕶️")
+    print("Inside Else")
+
+print("Completed")
+
+
+no_of_persons = 1
+
+if no_of_persons <= 2:
+    print("Lets go to party in bike 🛵")
+else:
+    print("Lets go to party in Car 🚘")
+
+
+is_raining = True
+is_sunny = False
+
+if is_raining:
+    print("Wear raincoat 🥼")
+elif is_sunny:
+    print("Wear shorts 🩳 & Glasses 🕶️")
+else:
+    print("Normal outfit 👚")
+
+
+# Task 1.1
+# Compare Two People’s Heights (Taller / Same Height)
+# Hint - input
+# Better - abs()
+# Expected Output -
+# Case 1:
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 163
+# Luffy is taller than Zoro by 10cm
+
+# Case 2:
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 185
+# Zoro is taller than Luffy by 12cm
+
+
+# Case 3
+# Please tell me the captain name?: Luffy
+# Please tell me the vice captain name?: Zoro
+# Please tell me the height of Luffy?: 173
+# Please tell me the height of Zoro?: 173
+# Luffy and Zoro are of same height
