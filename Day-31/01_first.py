321c3533d07cba40608c2cefb91cae1d6c59a63e
Nanthakumar <nnantha971@gmail.com>
2026-02-23 12:44:52 +0530

    the python class started day 1

---
+++ Day-31/01_first.py
@@ -0,0 +1,24 @@
+print("Hello")
+
+
+print(type(42))
+print(type(True))
+print(type(4.5))
+print(type([40,50,60]))
+
+full_name ="NK"
+print(full_name)
+print(type(full_name))
+
+# concatenation
+print ("welcome," + full_name)
+
+# task 1.1
+print(f"welcome,{full_name}")
+
+# task1.2
+age = 20
+print("My age is " + str(age))
+
+# task1.3
+print(f"My age is {age}")
