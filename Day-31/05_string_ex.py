203a1d5c9b3d04e30480852f35c1854a02fbf70e
Nanthakumar <nnantha971@gmail.com>
2026-02-23 16:46:05 +0530

    the string example 1.1 was completed

---
+++ Day-31/05_string_ex.py
@@ -0,0 +1,16 @@
+secret_message = "   Programming in Python is not only powerful but also fun!   "
+
+clean_message = secret_message.strip()
+print(f"{clean_message[15:21]}-{clean_message[34:43]}".upper())
+
+
+# Task 1.1
+# Expected Output
+# "PYTHON-POWERFUL"
+
+
+# Task 1.2
+flipped_message = "!nuf sseldnE dna seitinutroppo lufrewop htiw nohtyP"
+
+# Expected Output
+# "python 🗡️ powerful 🌸"
