478401ca563fa964ba469f15d1ad1980b2dfb638
Nanthakumar <nnantha971@gmail.com>
2026-02-24 10:47:30 +0530

    the loops

---
+++ Day-31/09_loops.py
@@ -0,0 +1,46 @@
+    # count = 10
+
+# while count >= 0:
+#     print(f"Counting 🐏 {count}")
+#     count = count - 1
+
+
+# count = 10
+
+# while count >= 0:
+#     print(f"Counting 🐏 {count}")
+#     count -= 1
+
+
+# count = 1
+# while count <= 10:
+#     print(f"Rep 🏋️  {count}")
+#     count += 1
+
+# start - 0
+# stop  - 9
+# for i in range(10):
+#     print(f"Rep 🏋️  {i}")
+
+# range(start, stop, step)
+# for i in range(1, 11, 1):
+#     print(f"Rep 🏋️  {i}")
+
+
+# count = 10
+
+# while count >= 0:
+#     print(f"Counting 🐏 {count}")
+#     count -= 1
+
+for i in range(10, 0, -1):
+    print(f"Counting 🐏 {i}")
+
+
+# Task 1.1
+# for Loop and while loop
+# 🔥
+# 🔥🔥
+# 🔥🔥🔥
+# 🔥🔥🔥🔥
+# 🔥🔥🔥🔥🔥
