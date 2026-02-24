07cd6d249b9f5cb210b5202a8f4574dea5e8999f
Nanthakumar <nnantha971@gmail.com>
2026-02-24 17:49:40 +0530

    the split and the join topics in python

---
+++ Day-32/07_split_join.py
@@ -0,0 +1,14 @@
+ice_creams = "mint|chocolate|vanilla"
+
+# String -> List
+# print(ice_creams.split("|"))
+
+print(ice_creams.split("o"))
+
+
+vegetables = ["🥕", "🥒", "🍆", "🥔"]
+
+
+print("|".join(vegetables))
+print("**".join(vegetables))
+print("🎉".join(vegetables))
