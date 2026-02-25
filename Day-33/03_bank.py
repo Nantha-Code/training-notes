06c563c9b3e4a2462c65b39fb3ac52cb552e993d
Nanthakumar <nnantha971@gmail.com>
2026-02-25 15:14:23 +0530

    interest task 1.6 is completed

--- Day-33/03_bank.py
+++ Day-33/03_bank.py
@@ -212,3 +212,74 @@ print(bala.apply_interest())
 # Your balance is: ₹10,200,000.00
 # Your balance is: ₹1,020,000.00
 # Your balance is: ₹5,100,000.00
+
+
+# Task 1.6
+# Account.update_interest_rate(4)
+# Clue: classmethod
+
+
+class Account:
+    # class variable
+    interest_rate = 0.04
+
+    def __init__(self, account_number, name, balance):
+        self.acc_no = account_number
+        self.name = name
+        self.balance = balance
+
+    # Task
+    def display_balance(self):
+        return f"Your balance is: ₹{self.balance:,.2f}"
+
+    def withdraw(self, amount):
+        # Shield 🛡️
+        if amount < 0:
+            return "Invalid amount"
+
+        if amount > self.balance:
+            return f"Insufficient funds. {self.display_balance()}"
+        # Shield 🛡️
+
+        self.balance = self.balance - amount
+        return f"Success. {self.display_balance()}"
+
+    def apply_interest(self):
+        self.balance = self.balance * Account.interest_rate + self.balance
+        return f"Success. Applied interest rate of {Account.interest_rate:.2%}. {self.display_balance()}"
+
+    def update_interest_rate():
+        pass
+
+
+gopi = Account(101, "Gopika Hariharan", 1_00_00_000)
+vikki = Account(102, "Vignesh M", 10_00_000)
+bala = Account(103, "Bala Kumar", 50_00_000)
+
+# print(Account.update_interest_rate(4))
+
+print(gopi.apply_interest())
+print(vikki.apply_interest())
+print(bala.apply_interest())
+
+# print(gopi.display_balance())
+# print(vikki.display_balance())
+# print(bala.display_balance())
+
+# # Success. Interest rate updated => 4.00%
+# # Success. Applied interest rate of 4.00%. Your balance is: ₹10,400,000.00
+# # Success. Applied interest rate of 4.00%. Your balance is: ₹1,040,000.00
+# # Success. Applied interest rate of 4.00%. Your balance is: ₹5,200,000.00
+# # Your balance is: ₹10,400,000.00
+# # Your balance is: ₹1,040,000.00
+# # Your balance is: ₹5,200,000.00
+
+# # Task 1.7
+# gopi = Account(101, "Gopika Hariharan", 1_00_00_000)
+# vikki = Account(102, "Vignesh M", 10_00_000)
+# bala = Account(103, "Bala Kumar", 50_00_000)
+# bala2 = Account(104, "Bala Kumar", 50_00_000)
+
+# print(Account.get_total_no_accounts())
+# # In total we have 4 accounts
+
